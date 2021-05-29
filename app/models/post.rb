2e647dfb31d05f7f092ca3b0afc81101b4fc1f91
TomaszKot11 <kottomasz98@gmail.com>
2021-05-29 18:53:53 +0200

    Relacja wiele do wielu

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     has_many :comments
+
+    has_many :categories_post
+    has_many :categories, through: :categories_post
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210529161608) do
+ActiveRecord::Schema.define(version: 20210529163717) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", limit: 255
+  end
+
+  create_table "categories_posts", force: :cascade do |t|
+    t.integer "post_id",     limit: 4
+    t.integer "category_id", limit: 4
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text    "content", limit: 65535
